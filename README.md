9b6742da81d45cb6e2cc1b80fe9006ae1be37c5b
lle-76 <test@test.com>
2021-04-21 09:05:53 +0200

    chore(): create project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Tic-Tac-Toe
+
+[Kata presentation](https://github.com/stephane-genicot/katas/blob/master/TicTacToe.md)
+
+## Technical Informations
+
+Language : Swift 5
+SDK : iOS 14.1
---
+++ Tic Tac Toe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tic Tac Toe
+//
+//  Created by Loïc Lefebvre on 21/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
